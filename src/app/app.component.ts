5c50a4442141b16a99b1011cdc3c85db43dfcff2
SrujanaDuvvuri <suji.duvvuri@gmail.com>
2021-02-10 23:49:31 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './models/country.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-forms-first';
+
+
+  countryList : Country[] = [
+    new Country(1,'India'),
+    new Country(2,'USA'),
+    new Country(3,'China'),
+    new Country(4,'Japan'),
+    new Country(5,'UK')
+  ]
+
+  onSubmit(contactForm : any){
+    console.log(contactForm.value)
+  }
 }
